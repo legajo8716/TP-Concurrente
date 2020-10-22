71ab47daf800943a74dbb69a9601735242b48725
legajo8716 <nelgonzalez88@gmail.com>
2020-10-22 13:56:37 -0300

    Inicio del Tp

---
+++ src/Buffer.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Buffer {
+    public static void Buffer(int dimension) {
+        ArrayList<Integer>cola=new ArrayList<Integer>(dimension);
+    }
+    
+}
